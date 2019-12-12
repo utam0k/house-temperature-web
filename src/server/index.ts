b97dbfc4eb35f87dd59f13b20122d6a64dcde362
Satoru Yokoyama <22640673+chao7150@users.noreply.github.com>
2019-12-13 00:53:33 +0900

    perf(server): use compression

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,8 +1,10 @@
 import * as express from "express";
 import * as path from "path";
+import compression from "compression";
 
 const app = express();
 
+app.use(compression());
 app.use(express.static(path.join(__dirname, "../dist")));
 app.get("/", (req, res) => {
   const options = {
