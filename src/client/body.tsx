49ab9e230cfeb9758c3a2c748172d666336843fe
utam0k <k0ma@utam0k.jp>
2019-12-23 01:35:01 +0900

    feat(src/client/body.tsx): グラフのレスポンシブル対応

--- src/client/body.tsx
+++ src/client/body.tsx
@@ -9,6 +9,7 @@ import {
   Tooltip,
   XAxis,
   YAxis,
+  ResponsiveContainer,
 } from "recharts";
 
 export interface Data {
@@ -43,32 +44,38 @@ export const App: React.FunctionComponent = () => {
         </a>
       </p>
       <h2>気温</h2>
-      <LineChart width={1000} height={400} data={data} syncId="chao">
-        <Line type="monotone" dataKey="temperature" />
-        <XAxis dataKey="time" interval={59} />
-        <YAxis domain={[0, 40]} />
-        <CartesianGrid />
-        <ReferenceLine y={17} label="↓違法" stroke="red" />
-        <Tooltip />
-      </LineChart>
+      <ResponsiveContainer width="90%" height={400}>
+        <LineChart data={data} syncId="chao">
+          <Line type="monotone" dataKey="temperature" />
+          <XAxis dataKey="time" interval={59} />
+          <YAxis domain={[0, 40]} />
+          <CartesianGrid />
+          <ReferenceLine y={17} label="↓違法" stroke="red" />
+          <Tooltip />
+        </LineChart>
+      </ResponsiveContainer>
       <h2>湿度</h2>
-      <LineChart width={1000} height={400} data={data} syncId="chao">
-        <Line type="monotone" dataKey="humidity" />
-        <XAxis dataKey="time" interval={59} />
-        <YAxis domain={[0, 70]} />
-        <CartesianGrid />
-        <ReferenceLine y={40} label="↓違法" stroke="red" />
-        <Tooltip />
-      </LineChart>
+      <ResponsiveContainer width="90%" height={400}>
+        <LineChart data={data} syncId="chao">
+          <Line type="monotone" dataKey="humidity" />
+          <XAxis dataKey="time" interval={59} />
+          <YAxis domain={[0, 70]} />
+          <CartesianGrid />
+          <ReferenceLine y={40} label="↓違法" stroke="red" />
+          <Tooltip />
+        </LineChart>
+      </ResponsiveContainer>
       <h2>気圧</h2>
-      <LineChart width={1000} height={400} data={data} syncId="chao">
-        <Line type="monotone" dataKey="pressure" />
-        <XAxis dataKey="time" interval={59} />
-        <YAxis domain={["dataMin", "dataMax"]} />
-        <CartesianGrid />
-        <ReferenceLine y={1013} label="標準気圧" />
-        <Tooltip />
-      </LineChart>
+      <ResponsiveContainer width="90%" height={400}>
+        <LineChart height={400} data={data} syncId="chao">
+          <Line type="monotone" dataKey="pressure" />
+          <XAxis dataKey="time" interval={59} />
+          <YAxis domain={["dataMin", "dataMax"]} />
+          <CartesianGrid />
+          <ReferenceLine y={1013} label="標準気圧" />
+          <Tooltip />
+        </LineChart>
+      </ResponsiveContainer>
     </>
   );
 };
